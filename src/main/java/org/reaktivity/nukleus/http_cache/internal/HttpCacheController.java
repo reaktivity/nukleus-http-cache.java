ba8c7c94768bcb291c799344ab35156078866f02
John Fallows <john.fallows@kaazing.com>
2018-04-02 22:17:42 -0700

    Support controller freeze

--- src/main/java/org/reaktivity/nukleus/http_cache/internal/HttpCacheController.java
+++ src/main/java/org/reaktivity/nukleus/http_cache/internal/HttpCacheController.java
@@ -28,6 +28,7 @@ import org.reaktivity.nukleus.Controller;
 import org.reaktivity.nukleus.ControllerSpi;
 import org.reaktivity.nukleus.function.MessageConsumer;
 import org.reaktivity.nukleus.function.MessagePredicate;
+import org.reaktivity.nukleus.http_cache.internal.types.control.FreezeFW;
 import org.reaktivity.nukleus.http_cache.internal.types.control.Role;
 import org.reaktivity.nukleus.http_cache.internal.types.control.RouteFW;
 import org.reaktivity.nukleus.http_cache.internal.types.control.UnrouteFW;
@@ -39,6 +40,7 @@ public final class HttpCacheController implements Controller
     // TODO: thread-safe flyweights or command queue from public methods
     private final RouteFW.Builder routeRW = new RouteFW.Builder();
     private final UnrouteFW.Builder unrouteRW = new UnrouteFW.Builder();
+    private final FreezeFW.Builder freezeRW = new FreezeFW.Builder();
 
     private final ControllerSpi controllerSpi;
     private final MutableDirectBuffer writeBuffer;
@@ -167,6 +169,17 @@ public final class HttpCacheController implements Controller
                                      .build();
 
         return controllerSpi.doUnroute(unroute.typeId(), unroute.buffer(), unroute.offset(), unroute.sizeof());
+    }
+
+    public CompletableFuture<Void> freeze()
+    {
+        long correlationId = controllerSpi.nextCorrelationId();
+
+        FreezeFW freeze = freezeRW.wrap(writeBuffer, 0, writeBuffer.capacity())
+                                  .correlationId(correlationId)
+                                  .build();
+
+        return controllerSpi.doFreeze(freeze.typeId(), freeze.buffer(), freeze.offset(), freeze.sizeof());
     }
 
     public long count(String name)
