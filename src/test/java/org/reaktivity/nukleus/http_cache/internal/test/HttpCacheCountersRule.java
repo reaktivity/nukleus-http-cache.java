1e259c7673b62b6512141ff9476ce7c10bfe5a8b
Akram Yakubov <akram.yakubov@kaazing.com>
2020-02-12 12:10:13 -0800

    Fix counters rule

--- src/test/java/org/reaktivity/nukleus/http_cache/internal/test/HttpCacheCountersRule.java
+++ src/test/java/org/reaktivity/nukleus/http_cache/internal/test/HttpCacheCountersRule.java
@@ -67,6 +67,11 @@ public class HttpCacheCountersRule implements TestRule
         return reaktor.counter("http-cache.responses.cached");
     }
 
+    public long requestToOrigin()
+    {
+        return reaktor.counter("http-cache.requests.cacheable.to.origin");
+    }
+
     public long responsesAborted()
     {
         return reaktor.counter("http-cache.responses.aborted");
@@ -177,7 +182,7 @@ public class HttpCacheCountersRule implements TestRule
     public void assertResponsesCached(
         int expected)
     {
-        assertEquals(expected, responsesCached());
+        assertEquals(expected, Math.max(responsesCached() - requestToOrigin(), 0));
     }
 
     public void assertResponsesAborted(
